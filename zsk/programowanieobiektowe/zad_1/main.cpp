1b98f65f4e2965101c805cff20ccbf7ad0f87cde
Mateusz Szortyka <90031811+Mszortyka@users.noreply.github.com>
2021-09-10 10:49:54 +0200

    Add files via upload

--- zsk/programowanieobiektowe/zad_1/main.cpp
+++ zsk/programowanieobiektowe/zad_1/main.cpp
@@ -3,32 +3,57 @@
 using namespace std;
 class Worker{
     public:
-        //deklaracja zmiennych cz≥onkowskich (pÛl, w≥aúciwosÊi , cech
+        //deklaracja zmiennych cz≈Çonkowskich (p√≥l, w≈Çaciwo≈õci , cech
     string name;
     string surname;
+    string nationality;
+    char gender;
 
-    //deklarcja (prototyp) metody cz≥onkowskiej
+    unsigned short int rokUrodzenia;
+    float wzrost;
+    //deklarcja (prototyp) metody cz¬≥onkowskiej
     void showSurname(){
-        cout<<"\nNazwisko pracownika:"<<surname;
+        cout<<"\n Nazwisko pracownika: "<<surname;
     };
     //deklaracja (prototyp ) metody
+    void showName()
+    {
+        cout<< "\n Imie pracownika: "<<name;
+    };
+    void showPersonality(){
+        cout<<"\n Imie i nazwisko pracownika: "<< name<< " "<< surname;
+    };
     void showAllData();
 };
+void Worker::showAllData(){
+        showPersonality();
+        cout<<"\n narodowsc pracownika: "<<nationality;
+        cout<<"\n Rok urodzenia: " << rokUrodzenia;
+        cout<<"\n Wzrost: " << wzrost;
+        cout<<"\n Plec: ";
 
-void Worker::showAllData()
-{
-    cout << "ImiÍ i Nazwisko pracownika:" << name << " "<<surname;
-};
+        switch(gender){
+            case 'm': cout<<"mezczyzna";
+            case 'k': cout<<"kobieta";
+            default: cout<<"-";
+        }
+    };
     int main()
 {
     setlocale(LC_CTYPE, "polish");
 
     Worker pracownik1;
+    pracownik1.name ="krystian";
     pracownik1.surname = "Nowak";
-    pracownik1.name = "Krystian";
+    pracownik1.rokUrodzenia = 2005;
+    pracownik1.wzrost = 180.0;
+    pracownik1.gender = 'm';
+    pracownik1.nationality = "polska";
 
+    pracownik1.showName();
+    pracownik1.showSurname();
+    pracownik1.showPersonality();
     pracownik1.showAllData();
-
 
 
     return 0;
