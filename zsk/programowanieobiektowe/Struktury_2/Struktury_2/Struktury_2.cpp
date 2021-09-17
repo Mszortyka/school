3ad03cc8964d23b0930403ee0a000f543de42c57
Mateusz Szortyka <90031811+Mszortyka@users.noreply.github.com>
2021-09-17 11:25:31 +0200

    Add files via upload

---
+++ zsk/programowanieobiektowe/Struktury_2/Struktury_2/Struktury_2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Date {
+	unsigned short int dd, mm, rrrr;
+};
+
+class Worker {
+public:
+	unsigned int id{}; // incjalizujemy domyślnie int = 0 
+	string name{}, surname{};
+	Date dateBirhthday;
+	void showAllData();
+	void setData(unsigned int x, string setName,string setSurname, Date setdateBirthday);
+};
+
+void Worker::showAllData(){   // operator zakresu
+	cout << "Dane pracownika: \nIdentyfikator: " << id;
+	cout << "\nimię: " << name;
+	cout << "\nnazwisko: " << surname;
+
+	cout << "\ndata urodzenia: " << dateBirhthday.dd << "." << dateBirhthday.mm << "." << dateBirhthday.rrrr;
+}
+
+void Worker::setData(unsigned int x ,string setName, string setSurname, Date setdateBirthday ){
+	id = x;
+	name = setName;
+	surname = setSurname;
+	//dateBirhthday.dd = setDay;
+	//dateBirhthday.mm = setMonth;
+	//dateBirhthday.rrrr = setYear;
+
+
+	//Worker::id = id
+	//Worker::dateBirhthday.mm = setMonth
+
+
+	dateBirhthday = setdateBirthday;
+
+}
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	Worker kowalski;
+	kowalski.setData(10, "Jan", "kowalski", {17,9,2021});
+	kowalski.showAllData();
+	
+
+
+
+}
+
