0ad117f70ec7165ee0efda552c25d7b6a997b0f1
Mateusz Szortyka <90031811+Mszortyka@users.noreply.github.com>
2021-10-29 11:23:51 +0200

    Add files via upload

---
+++ zsk/programowanieobiektowe/inicjalizacja_zmiennych_członkowskich.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+class Car {
+public:
+	unsigned int id;
+	string brand;
+	string model;
+
+	void getData();
+
+	Car();
+	Car(unsigned short pId, string pBrand, string pModel);
+	Car(string pBrand);
+};
+
+void Car::getData() {
+	cout << "\nId: " << id << "\n marka i model: " << brand << " " << model << endl;
+}
+
+Car::Car():
+	id{0}, 
+	brand{"MARKA"},
+	model{"MODEL"}
+{
+	cout << "\n konstruktor domyślny" << endl;
+};
+Car::Car(string pBrand) {
+	brand = pBrand;
+}
+Car::Car(unsigned short pId, string pBrand, string pModel) {
+	id = id;
+	brand = pBrand;
+	model = pModel;
+}
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	
+	Car fiat;
+	fiat.getData();
+
+	Car fiat1 = Car();
+	fiat1.getData();
+
+	Car fiat2(10, "Fiat", "Multipla");
+	fiat2.getData();
+
+	Car fiat3 = Car(20, "Fiat", "Panda");
+	fiat3.getData();
+
+	return 0;
+};
