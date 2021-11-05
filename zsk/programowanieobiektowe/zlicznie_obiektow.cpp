4ec5f645c1ecdf4a07c136edd977d8a1b70d7109
Mateusz Szortyka <90031811+Mszortyka@users.noreply.github.com>
2021-11-05 11:10:37 +0100

    Add files via upload

---
+++ zsk/programowanieobiektowe/zlicznie_obiektow.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Worker {
+public:
+    static unsigned int i;
+    Worker();
+    ~Worker() {
+        i--;
+    }
+
+    static void howMany() {
+        cout << "Jest: " << i << " obiektów" << endl;
+    }
+};
+unsigned int Worker::i = 0;
+
+Worker::Worker() {
+    i++;
+};
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+
+    Worker worker;
+    Worker worker1;
+    Worker worker2;
+    Worker worker3;
+    Worker worker4;
+    Worker worker5;
+    Worker worker6;
+    Worker worker7;
+    Worker* p_worker;
+    Worker worker8;
+
+    Worker::howMany();
+    return 0;
+}
